eb8c1e5975da9dce24a3b1600932bcf694bfe245
Gabriel Ng <gabrielng15@gmail.com>
2018-12-10 16:00:40 -0800

    Set up fetch call to musix API in Provider

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 
+import { Provider } from './context';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (<Router>
+    return (<Provider>
+      <Router>
         <React.Fragment>
           <Navbar />
           <div className="container">
@@ -18,6 +21,7 @@ class App extends Component {
           </div>
         </React.Fragment>
       </Router>
+    </Provider>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
 // Our Provider where we hold the state that we will be sharing
 export class Provider extends Component {
     state = {
-        track_list: [
-            {
-                track: {
-                    track_name: "abc"
-                }
-            },
-            {
-                track: {
-                    track_name: "123"
-                }
-            },
-            {
-                track: {
-                    track_name: "xyz"
-                }
-            },
-        ],
+        track_list: [],
         heading: "Top 10 Tracks Baby"
+    }
+    
+    componentDidMount = async () => {
+        let response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`);
+
+        let parsedResponse = await response.json();
+
+        console.log(parsedResponse);
+
+        this.setState({
+            track_list: parsedResponse.message.body.track_list
+        })
+       
+
     }
 
     render() {
