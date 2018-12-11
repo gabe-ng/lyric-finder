ef5d73d2b83641af03f4edbf1606ad791acdf1bf
Gabriel Ng <gabrielng15@gmail.com>
2018-12-10 18:49:40 -0800

    Updates

---
+++ precache-manifest.cfac726b981f55a806a4557030bc3d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bce768cf37c4225ba4",
+    "url": "/lyric-finder/static/css/main.03ce951c.chunk.css"
+  },
+  {
+    "revision": "79bce768cf37c4225ba4",
+    "url": "/lyric-finder/static/js/main.79bce768.chunk.js"
+  },
+  {
+    "revision": "1198dbfa6a2975999ed7",
+    "url": "/lyric-finder/static/js/1.1198dbfa.chunk.js"
+  },
+  {
+    "revision": "7a31bb6fc7faa8b49216",
+    "url": "/lyric-finder/static/js/runtime~main.7a31bb6f.js"
+  },
+  {
+    "revision": "3aac9d86c7f997ad347670f1d541809a",
+    "url": "/lyric-finder/index.html"
+  }
+];
